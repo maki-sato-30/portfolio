03acc7674f85e42733262e58bdba63fb0c5ba238
maki-sato-30 <127070010+maki-sato-30@users.noreply.github.com>
2023-12-19 16:36:04 +0900

    Update original.js

--- js/original.js
+++ js/original.js
@@ -1,14 +1,39 @@
-$(function() {
-    $('.works-slider').slick({
-        autoplay: true,
-        fade: true,
-        prevArrow: '<img src="img/prev.svg" class="slide-arrow prev-arrow">',
-        nextArrow: '<img src="img/next.svg" class="slide-arrow next-arrow">'
-    });
+//スライダーの設定
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: false,
+    dots: true,
+    fade: true,
+    speed: 1500,
+    dotsClass: "slide-dots",
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
 });
 
+// ハンバーガー
+$('.h-menu-toggle').on('click', function () {
+    $('.h-menu-toggle, .h-menu').toggleClass('show');
+    $('html').toggleClass('scroll');
+});
 
-//hover無効
+//ページ内リンクスムーススクロール
+$('a[href^="#"]').on('click', function () {
+    // .h-menuと.h-menu-toggleのクラス'show'を取り除く
+    $('.h-menu, .h-menu-toggle').removeClass('show');
+    // スクロール制限を解除
+    $('html').removeClass('scroll');
+    
+    // 以下は既存のスムーススクロールの処理
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop: position}, 550, "swing");
+    return false;
+});
+
+//タッチデバイスhover無効
 addEventListener('DOMContentLoaded', function() {
     if (isTouchDevice()) {
         for (var i in document.styleSheets)
@@ -40,55 +65,14 @@ addEventListener('DOMContentLoaded', function() {
     }
 });
 
-//ページ内リンクスムーススクロール
-$('a[href^="#"]').on('click', function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop: position}, 550, "swing");
-    return false;
-});
-
-//アニメーション
+//fadeInアニメーション
 $(function () {
     $(window).scroll(function () {
         $('.fade-in').each(function () {
             var position = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            if (scroll > position - windowHeight + 20) {
-                $(this).addClass('active');
-            }
-        });
-        $('.fade-top').each(function () {
-            var position = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > position - windowHeight + 20) {
-                $(this).addClass('active');
-            }
-        });
-        $('.fade-bottom').each(function () {
-            var position = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > position - windowHeight + 20) {
-                $(this).addClass('active');
-            }
-        });
-        $('.fade-left').each(function () {
-            var position = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > position - windowHeight + 20) {
-                $(this).addClass('active');
-            }
-        });
-        $('.fade-right').each(function () {
-            var position = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > position - windowHeight + 20) {
+            if (scroll > position - windowHeight + 200) {
                 $(this).addClass('active');
             }
         });
